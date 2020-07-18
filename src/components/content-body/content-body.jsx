990c4ac9ff2f3d7da28c2b79b283dba1ab10e2a6
rishabh kalra <rishabh.kalra@niit.com>
2020-07-18 15:49:01 +0530

    added selective banners

--- src/components/content-body/content-body.jsx
+++ src/components/content-body/content-body.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import './content-body.scss';
+import {ContentBodyService} from './../../services/content-body-service'
+import SlideShow from '../shared/slide-show/slide-show';
 
 export class ContentBody extends React.Component {
 
-    render(props) {
-        return (null)
+    constructor(props) {
+        super(props);
+        this.state = {
+            bannerUrls: []
+        }
+    }
+
+    componentDidMount() {
+       const urls =  ContentBodyService.slideUrls()
+       this.setState({
+           bannerUrls: urls
+       })
+    }
+
+    getBannerTemplates = (urls) => {
+        return urls.map((url,idx) => {
+            return <SlideShow source={url} key={"slider_" + idx} />
+        })
+    }
+
+    render() {
+        return (
+        <React.Fragment>
+            <div className="content-container">
+            {this.state.bannerUrls.length && this.getBannerTemplates(this.state.bannerUrls)}
+            </div>
+        </React.Fragment>
+        )
     }
 }
 
---
+++ src/components/shared/slide-show/slide-show.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import {ContentBodyService} from './../../../services/content-body-service'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import './slide-show.scss'
+
+export default class slideShow extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentSlide: 0,
+            sliderTemplate: null,
+        }
+    }
+
+    async componentDidMount() {
+        if (this.props.source) {
+            try {
+                const response = await ContentBodyService.fetchSliderImages(this.props.source, true)
+                if (Array.isArray(response) && response.length) {
+                    const parsedImages = ContentBodyService.parseResponseForSliders(response)
+                    const template = this.getSlider(parsedImages)
+                        this.setState({
+                            sliderTemplate: template
+                        })
+                }
+            } catch(sliderErr) {
+                console.error('An error occured while rendering slider using the url ' + this.props.source)
+            }
+        }
+    }
+
+    visitSlideRoute = (image) => {
+        console.log('image clicked is ', image)
+    }
+
+    getSlider = (images) => {
+        const option = {
+            showIndicators: false,
+            autoPlay: true,
+            showThumbs: false,
+            showArrows: false,
+            showStatus: false,
+            transitionTime: '700',
+            interval: '5000',
+            infiniteLoop: true,
+            swipeable: true,
+            emulateTouch: true,
+        }
+        return <Carousel 
+                    {...option}
+                    >
+            {images.map(image => {
+                let color= {backgroundColor : image.color}
+            return <div 
+            className="carousel-image-container" style= {color}
+            key={image.id}
+            onClick={() => this.visitSlideRoute(image)}
+            >
+                <img src={image.url} alt={`banner_image ${image.alt}`} />
+            </div>
+        })}
+        </Carousel>
+    }
+    render() {
+        return (
+            this.props?.source &&
+            <div className="slideshow-container">
+                <div className="slideshow-wrapper">
+                    {this.state.sliderTemplate}
+                </div>
+            </div>
+        )
+    }
+}
