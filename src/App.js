04cb70b7802a3c126e75a80340bb0d85cf12f83d
rishabh kalra <rishabh.kalra@niit.com>
2020-07-21 13:09:35 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './tailwind.output.css';
 import './App.scss';
 import { Header } from './components/header/header';
 import { ContentBody } from './components/content-body/content-body';
