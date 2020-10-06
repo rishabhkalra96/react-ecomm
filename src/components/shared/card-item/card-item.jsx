e8edda6d03c051edd5121221535d8a8441471b7d
rishabh kalra <rishabh.kalra@niit.com>
2020-10-06 20:23:52 +0530

    converted to functional component

--- src/components/shared/card-item/card-item.jsx
+++ src/components/shared/card-item/card-item.jsx
@@ -1,23 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import './card-item.scss'
+import defaultImage from './image_NA.png'
 
-export default class CardItem extends Component {
-    render() {
-        return (
-            <div className="cardItem-wrapper">
-                <div className="cardItem-container">
-                    <div className="cardItem-card-top">
-                        <div className="cardItem-card-top-micro"></div>
-                        <div className="cardItem-card-top-macro"></div>
-                    </div>
-                    <div className="cardItem-card-body">
-                        <div className="cardItem-card-body-desc"></div>
-                        <div className="cardItem-card-body-creator"></div>
-                        <div className="cardItem-card-body-micro"></div>
-                        <div className="cardItem-card-body"></div>
+export default function CardItem(props) {
+    const [productImage, setProductImage] = useState(defaultImage)
+
+    useEffect(() => {
+        if (props.imageUrl) {
+            setProductImage(props.imageUrl)
+        }
+
+    }, [props.imageUrl])
+    return (
+        <div className="cardItem-wrapper">
+            <div className="cardItem-container">
+                <div className="cardItem-card-top">
+                    <div className="cardItem-card-top-micro"></div>
+                    <div className="cardItem-card-top-macro">
+                        <img src={productImage} alt="product-representation"/>
                     </div>
                 </div>
+                <div className="cardItem-card-body">
+                    <div className="cardItem-card-body-desc"></div>
+                    <div className="cardItem-card-body-creator"></div>
+                    <div className="cardItem-card-body-micro"></div>
+                    <div className="cardItem-card-body"></div>
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
