c22ceb0c5ce95b59c49c4356a358544f458abc4b
rishabh kalra <rishabh.kalra@niit.com>
2020-10-06 21:06:42 +0530

    updated card-carousel as functional component

--- src/components/content-body/content-body.jsx
+++ src/components/content-body/content-body.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './content-body.scss';
 import { ContentBodyService } from './../../services/content-body-service'
 import SlideShow from '../shared/slide-show/slide-show';
-import CardCarousel from '../shared/card-carousel/card-carousel';
+import {CardCarousel} from '../shared/card-carousel/card-carousel';
 import { useState } from 'react';
 
 export const ContentBody = () => {
