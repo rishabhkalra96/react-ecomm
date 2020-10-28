a718ce8197bed791d595ad0dac6810397cca912f
rishabh kalra <rishabh.kalra@niit.com>
2020-10-28 23:34:47 +0530

    added user ratings feature ui

--- src/components/shared/card-item/card-item.jsx
+++ src/components/shared/card-item/card-item.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import './card-item.scss'
 import defaultImage from './image_NA.png'
+import Ratings from './../ratings/ratings'
 
 export default function CardItem(props) {
     const [productImage, setProductImage] = useState(defaultImage)
@@ -13,8 +14,17 @@ export default function CardItem(props) {
         }
 
     }, [props.imageUrl])
+
+    const handleHover = (e) => {
+        e.preventDefault()
+        // start the slideshow and make the images full width
+    }
+
+    const handleRatingsClick = (e) => {
+        console.log('clicked on ratings', e)
+    }
     return (
-        <div className="cardItem-wrapper">
+        <div className="cardItem-wrapper" onMouseOver={handleHover}>
             <div className="cardItem-container">
                 <div className="cardItem-card-top">
                     <div className="cardItem-card-top-micro"></div>
@@ -23,10 +33,16 @@ export default function CardItem(props) {
                     </div>
                 </div>
                 <div className="cardItem-card-body">
-                    <div className="cardItem-card-body-desc"></div>
-                    <div className="cardItem-card-body-creator"></div>
+                        <p className="cardItem-card-body-desc">
+                        <b>A perfect image for your home</b>
+                        </p>
+                    <p className="cardItem-card-body-creator">
+                        <span>By </span> Artistic Boys
+                    </p>
+                    <div className="cardItem-ratings">
+                        <Ratings count={5} filled={Math.floor((Math.random() * 5) + 1)} hoverEffect={true} ratingsCount={Math.floor((Math.random() * 500) + 1)} clickHandler={handleRatingsClick}/>
+                    </div>
                     <div className="cardItem-card-body-micro"></div>
-                    <div className="cardItem-card-body"></div>
                 </div>
             </div>
         </div>
---
+++ src/components/shared/ratings/ratings.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import './ratings.scss'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    padding: {
+      padding: theme.spacing(0),
+    }
+  }));
+
+export default function Ratings(props) {
+    const [filledStars, setFilledStars] = useState(0);
+    const [hasRated, setHasRated] = useState(false)
+    const [totalRatings, setTotalRatings] = useState(0)
+    const materialClasses = useStyles();
+    useEffect(() => {
+        if (props.filled) {
+            setFilledStars(props.filled)
+        }
+    }, [props.filled])
+
+    useEffect(() => {
+        if (props.ratingsCount) {
+            setTotalRatings(props.ratingsCount)
+        }
+    }, [props.ratingsCount])
+
+    const renderStars = () => {
+        const stars = new Array(props.count).fill(0)
+        return stars.map((_, i) => {
+            let classes = 'star'
+            if (props.filled && i < filledStars) {
+                classes += ' filled'
+            }
+            return <div className={classes} key={i} number={i+1}>
+                <FontAwesomeIcon icon={faStar} onClick={ratingClickHandler}/>
+            </div>
+        })
+    }
+
+    const ratingClickHandler = (e) => {
+        const currentVal = e.target.parentElement.parentElement.getAttribute('number') ? parseInt(e.target.parentElement.parentElement.getAttribute('number')) : 0
+        if (currentVal !== props.filled) {
+            setFilledStars(currentVal)
+            setHasRated(true)
+        }
+        props.clickHandler({
+            item: {},
+            initialVal: props.filled,
+            currentVal
+        })
+    }
+
+    const removeCurrentRating = (e) => {
+        if (hasRated) {
+            setFilledStars(props.filled)
+            props.clickHandler({
+                item: {},
+                initialVal: props.filled,
+                currentVal: props.filled,
+            })
+            // to let the ripple effect finish on the undo button
+            window.setTimeout(() => {
+                setHasRated(false)
+            }, 300)
+        }
+    }
+    return (
+        <div className="ratings-core">
+            {renderStars()}
+            <span className="ratings-count">
+                {props.ratingsCount ? totalRatings + ' user ratings' : null}
+            </span>
+            {
+                hasRated ? <p className="ratings-remova"
+                onClick={removeCurrentRating}
+            >
+                <Button size="small" color="secondary" className={materialClasses.padding}>
+          Undo
+        </Button>
+            </p> : null    
+            }
+        </div>
+    )
+}
